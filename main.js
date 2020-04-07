5d8e8b1570f41b8624b4bc7da879bb560e422f44
zac <zac@nivas.hr>
2020-04-07 19:26:57 +0200

    Background, shadow, color

--- main.js
+++ main.js
@@ -2,15 +2,9 @@
 ;(function ($) {
   "use strict";
   
-  let contactCard  = document.getElementsByClassName('contact-card')[0],
-  cName = contactCard.getAttribute('data-toggle'),
-  cardTranslation = -2.5,
-  cardRotation = -4.5,
-  intervalMoveCard;
-  let arrow = document.getElementsByClassName('scroll-arrow')[0],
-  moveArrow = 0;
-  let menu = document.getElementsByClassName('menu-toggle')[0];
-  
+  let contactCard = document.getElementsByClassName('contact-card')[0],
+//   contactCardShadow = document.getElementsByClassName('contact-card-shadow')[0],
+  cName = contactCard.getAttribute('data-toggle');  
   let isFirstClick = true;
   
   if (!contactCard) return;
@@ -21,13 +15,7 @@
   // - Show the scroll indicator arrow
   contactCard.addEventListener('click', function (event) {
     
-    if (isFirstClick) {
-      // Stop wiggling the card around
-      clearInterval(intervalMoveCard);
-      contactCard.style.transform = '';
-      // Make the transitions quicker, for flipping
-      contactCard.style.transition = 'transform 0.70s cubic-bezier(0.49, 0.23, 0.58, 0.49)';
-      
+    if (isFirstClick) {    
       // Initialize tooltips, show on hover
       $(function () {
         $('[data-toggle="tooltip"]').tooltip({
@@ -44,6 +32,7 @@
     
     let toggled = contactCard.classList.contains(cName);
     contactCard.classList[toggled ? 'remove' : 'add'](cName);
+    // contactCardShadow.classList[toggled ? 'remove' : 'add'](cName);
     
   }); 
   
